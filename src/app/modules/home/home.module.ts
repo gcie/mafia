4bfdfd66d1dadc3458e3232d64e282608ff8296c
Grzegorz Ciesielski <tao24.k@gmail.com>
2021-07-28 13:27:01 +0200

    missing auth guard

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/core/guards/auth.guard';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MaterialModule } from '../material.module';
 import { HomeComponent } from './home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: '', component: HomeComponent, canActivate: [AuthGuard] }];
 
 @NgModule({
   declarations: [HomeComponent],
